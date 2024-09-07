aa9c4c25172ab364bf8598002b0f4a4239ff2c7e
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2024-09-07 19:11:46 +0530

    feat: add RestAPI index file

---
+++ RestAPI/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/msg", (req, res, next) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.use(express.json());
+
+app.post("/msg", (req, res, next) => {
+  const message = req.body.message;
+  res.json({ receivedMessage: message });
+});
+
+class Message {
+  constructor(content) {
+    this.content = content;
+  }
+
+  getContent() {
+    return this.content;
+  }
+}
+
+app.post("/msg", (req, res, next) => {
+  const newMessage = new Message(req.body.message);
+  res.json({ receivedMessage: newMessage.getContent() });
+});
